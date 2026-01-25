9baafada7807f375904ec535588a2e9f9de5bc00
Welton Nogueira <welton@internit.net.br>
2026-01-24 23:56:47 -0300

    changes

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+console.log("PREFIX", process.env.PREFIX);
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: process.env.PREFIX,
+  assetPrefix: process.env.PREFIX,
 };
 
 export default nextConfig;
